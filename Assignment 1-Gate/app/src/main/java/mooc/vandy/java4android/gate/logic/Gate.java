a2c02c0418b7cd2107bdaac30899132843eaa251
Vidish Naik <Vidish Naik>
2016-11-22 23:06:11 +0530

    1st Submission

--- Assignment 1-Gate/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ Assignment 1-Gate/app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -5,5 +5,52 @@ package mooc.vandy.java4android.gate.logic;
  */
 public class Gate {
     // TODO -- Fill in your code here
-    
+    public static int IN = 1;
+    public static int OUT = -1;
+    public static int CLOSED = 0;
+    private int mSwing;
+
+    public Gate()
+    {
+        mSwing = CLOSED;
+    }
+    public boolean setSwing(int direction)
+    {
+        if(direction != mSwing && direction >= -1 && direction <= 1)
+        {
+            mSwing = direction;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean open(int direction) {
+        return setSwing(direction);
+    }
+
+    public void close()
+    {}
+
+    public int getSwingDirection() {
+        return mSwing;
+    }
+    public int thru(int count)
+    {
+        return getSwingDirection() * count;
+    }
+
+    @Override
+    public String toString() {
+        switch (getSwingDirection())
+        {
+            case -1 :
+                return "This gate is open and swings to exit the pen only";
+            case 0 :
+                return "This gate is closed";
+            case 1 :
+                return "This gate is open and swings to enter the pen only";
+            default :
+                return "This gate has an invalid swing direction";
+        }
+    }
 }
