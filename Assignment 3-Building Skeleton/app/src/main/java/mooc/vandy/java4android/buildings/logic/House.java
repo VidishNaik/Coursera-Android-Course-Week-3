f4344ad0e4ea60f2805933f3d644f1776920b88b
Vidish Naik <Vidish Naik>
2016-11-23 19:10:53 +0530

    Finished House Class

--- Assignment 3-Building Skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
+++ Assignment 3-Building Skeleton/app/src/main/java/mooc/vandy/java4android/buildings/logic/House.java
@@ -7,6 +7,57 @@ public class House
        extends Building {
        
     // TODO - Put your code here.
-    
-    
+    private String owner;
+    private boolean pool;
+
+    public House(int length,int width,int lotlength,int lotwidth)
+    {
+        super(length,width,lotlength,lotwidth);
+    }
+
+    public House(int length,int width,int lotlength,int lotwidth,String owner)
+    {
+        this(length,width,lotlength,lotwidth);
+        this.owner = owner;
+    }
+
+    public House(int length,int width,int lotlength,int lotwidth,String owner,boolean pool)
+    {
+        this(length,width,lotlength,lotwidth,owner);
+        this.pool=pool;
+    }
+
+    public void setOwner(String owner)
+    {
+        this.owner = owner;
+    }
+
+    public void setPool(boolean pool)
+    {
+        this.pool = pool;
+    }
+
+    @Override
+    public String toString() {
+        String sent = "Owner: ";
+        if(this.owner.equals("") || this.owner == null)
+            sent = sent + "n/a; ";
+        else
+            sent = sent + owner + "; ";
+        if(pool)
+            sent = sent + "has a pool; ";
+        if(calcLotArea() > calcBuildingArea())
+            sent = sent + "has a big open space; ";
+        return sent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof House)
+        {
+            if(((House) o).calcBuildingArea() == this.calcBuildingArea() && ((House) o).pool)
+                return true;
+        }
+        return false;
+    }
 }
