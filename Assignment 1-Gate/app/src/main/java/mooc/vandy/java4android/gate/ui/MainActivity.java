84106cf382988942bdafeb69c3f2f7cdc9620b86
Vidish Naik <vidishnaik0509@gmail.com>
2021-10-13 23:10:49 -0700

    Bump gradle version

--- Assignment 1-Gate/app/src/main/java/mooc/vandy/java4android/gate/ui/MainActivity.java
+++ Assignment 1-Gate/app/src/main/java/mooc/vandy/java4android/gate/ui/MainActivity.java
@@ -1,6 +1,6 @@
 package mooc.vandy.java4android.gate.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
